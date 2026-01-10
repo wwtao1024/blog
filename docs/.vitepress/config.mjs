67822a54e464f3fd35b21c8e63d0fb2c9bcd7a2b
wuwentao <1611839484@qq.com>
2026-01-10 16:21:13 +0800

    feat(config): 添加子路径配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/blog/",
   title: "Forge Code",
   description: "Based on vitepress's blog",
   themeConfig: {
