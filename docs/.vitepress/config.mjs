b24f5d06e324db7212edd736575a0f8b6deb2a25
wuwentao <1611839484@qq.com>
2026-01-11 15:03:56 +0800

    feat(ui): 添加 Teek 主题支持 ci(gitlab): 调整安装依赖为pnpm

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "vitepress";
+import { defineTeekConfig } from "vitepress-theme-teek/config";
+
+// Teek 主题配置
+const teekConfig = defineTeekConfig({});
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  extends: teekConfig,
   // 站点目录路径
   base: "/blog/",
   // 站点标题
