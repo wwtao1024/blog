13a56ad3eb072833cfe248d0942326e80160ff68
wuwentao <1611839484@qq.com>
2026-01-11 16:49:56 +0800

    chore(config): 调试Teek主题配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,9 +2,43 @@ import { defineConfig } from "vitepress";
 import { defineTeekConfig } from "vitepress-theme-teek/config";
 
 // Teek 主题配置
-const teekConfig = defineTeekConfig({});
+const teekConfig = defineTeekConfig({
+  vpHome: true,
+  // 深色、浅色模式切换过渡动画配置。
+  viewTransition: {
+    enabled: true, // 是否启用深浅色切换动画效果
+    mode: "out-in", // 动画模式，out 始终从点击点往全屏扩散，out-in 第一次从点击点往全屏扩散，再次点击从全屏回到点击点
+    duration: 300, // 动画持续时间，当 mode 为 out 时，默认为 300ms，mode 为 out-in 时，默认为 600ms
+    easing: "ease-in", // 缓动函数
+  },
+  codeBlock: {
+    enabled: true, // 是否启用新版代码块
+    collapseHeight: 700, // 超出高度后自动折叠，设置 true 则默认折叠，false 则默认不折叠
+    overlay: true, // 代码块底部是否显示展开/折叠遮罩层
+    overlayHeight: 400, // 当出现遮罩层时，指定代码块显示高度，当 overlay 为 true 时生效
+    langTextTransform: "uppercase", // 语言文本显示样式，为 text-transform 的值:none, capitalize, lowercase, uppercase
+    copiedDone: (TkMessage) => TkMessage.success("复制成功！"), // 复制代码完成后的回调
+  },
+  // 是否启用侧边栏展开/折叠触发器
+  sidebarTrigger: true,
+  author: {
+    name: "wu wen tao", // 作者名称
+    link: "https://github.com/wwtao1024", // 点击作者名称后跳转的链接
+  },
+  // 布局模式
+  themeEnhance: {
+    // 布局模式
+    layoutSwitch: {
+      defaultMode: "fullWidth",
+    },
+    // 主题色
+    themeColor: {
+      defaultColorName: "ep-green",
+    },
+  },
+});
 
-// https://vitepress.dev/reference/site-config
+// VitePress 配置
 export default defineConfig({
   extends: teekConfig,
   // 站点目录路径
--- docs/index.md
+++ docs/index.md
@@ -3,7 +3,7 @@
 layout: home
 
 hero:
-  name: ""
+  name: "你好啊"
   text: "Forge Code"
   tagline: 从框架整合到工程优化，探索技术融合的更多可能。
   image:
@@ -24,4 +24,18 @@ features:
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
   - title: Feature C
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+# tk:
+#   blogger:
+#     name: 天客
+#     slogan: 朝圣的使徒，正在走向编程的至高殿堂！
+#     avatar: https://testingcf.jsdelivr.net/gh/Kele-Bingtang/static/user/avatar1.png
+#     shape: circle-rotate
+#     circleBgImg: /blog/bg4.webp
+#     circleBgMask: true
+#     circleSize: 100
+#     color: #ffffff
+#     status:
+#       # icon: "😪",
+#       size: 24,
+#       title: "困"
 ---
